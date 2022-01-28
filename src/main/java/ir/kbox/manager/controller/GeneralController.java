95036aa1dfd366cbacd78b509b1138354157df05
Mehran Ghajari <mehranghajari>
2022-01-29 01:03:29 +0330

    Fix registering form

--- src/main/java/ir/kbox/manager/controller/GeneralController.java
+++ src/main/java/ir/kbox/manager/controller/GeneralController.java
@@ -1,14 +1,21 @@
 package ir.kbox.manager.controller;
 
+import ir.kbox.manager.model.user.User;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class GeneralController {
+
     @GetMapping(path = {"/", "/index.html", "/index", "/home"})
     public String getIndexPage() {
         return "index";
     }
 
-
+    @GetMapping(path= {"/adduser"})
+    @PreAuthorize("hasRole('ADMIN')")
+    public  String getAddUserPage(User user) {
+        return "adduser";
+    }
 }
